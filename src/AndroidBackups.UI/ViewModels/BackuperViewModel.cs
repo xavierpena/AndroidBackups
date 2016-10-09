f7c84762217ce77d92cda8a4c8b578e2ded98962
Xavier Peña <xavierpenya@gmail.com>
2016-10-09 22:40:52 +0200

    Treeview (not working yet)

--- src/AndroidBackups.UI/ViewModels/BackuperViewModel.cs
+++ src/AndroidBackups.UI/ViewModels/BackuperViewModel.cs
@@ -8,6 +8,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace AndroidBackups.UI.ViewModels
@@ -26,6 +27,36 @@ namespace AndroidBackups.UI.ViewModels
                 {
                     _devices = value;
                     OnPropertyChanged(nameof(Devices));
+                }
+            }
+        }
+
+        private PortableDevice _selectedDevice;
+        public PortableDevice SelectedDevice
+        {
+            get { return _selectedDevice; }
+            set
+            {
+                if (_selectedDevice != value)
+                {
+                    _selectedDevice = value;                    
+                    OnPropertyChanged(nameof(SelectedDevice));
+
+                    InitTreeView();
+                }
+            }
+        }
+
+        private TreeViewItem _rootFileItem;
+        public TreeViewItem RootFileItem
+        {
+            get { return _rootFileItem; }
+            set
+            {
+                if (_rootFileItem != value)
+                {
+                    _rootFileItem = value;
+                    OnPropertyChanged(nameof(RootFileItem));
                 }
             }
         }
@@ -51,11 +82,53 @@ namespace AndroidBackups.UI.ViewModels
         {
             // Get the only connected MTP device:                
             var service = new StandardPortableDevicesService();
-            this.Devices = service.Devices;
+            var devices = service.Devices;
+
+            // Add "null" in front of them:
+            var devicesForUi = new List<PortableDevice>() { null };
+            foreach (var device in devices)
+                devicesForUi.Add(device);
+
+            this.Devices = devicesForUi;
+
             //InitSourceFolders();
 
             // Delegates:
             //_openConfigurationFileCommand = new DelegateCommand(() => _episodeRepository.OpenConfigurationFile());
+        }
+
+        private void InitTreeView()
+        {
+            _selectedDevice.Connect();
+            var rootFolder = _selectedDevice.GetFullContents();
+            _selectedDevice.Disconnect();
+
+            this.RootFileItem = GetTreeviewItems(rootFolder)
+                .First();
+        }
+
+        private List<TreeViewItem> GetTreeviewItems(PortableDeviceFolder portableDeviceFolder)
+        {
+            var treeViewItems = new List<TreeViewItem>();
+            foreach (var element in portableDeviceFolder.Files)
+            {
+                var treeViewItem = new TreeViewItem();
+                if (element.GetType() == typeof(PortableDeviceFolder))
+                {
+                    treeViewItem.Header = "\\" + element.Name;
+                    
+                    var subFolder = (PortableDeviceFolder)element;
+                    var subTreeViewItems = GetTreeviewItems(subFolder);
+                    foreach (var subTreeViewItem in subTreeViewItems)
+                        treeViewItem.Items.Add(subTreeViewItem);
+                }
+                else
+                {
+                    treeViewItem.Header = element.Name;
+                }
+                treeViewItems.Add(treeViewItem);
+            }
+            return treeViewItems;
         }
 
         private void InitSourceFolders()
